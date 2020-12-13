ff2c004765089a3489d16e9f9d66bcd761be7e08
Ryan Larkin <rlarkin212@gmail.com>
2020-12-13 18:24:07 +0000

    works with pngs now

--- src/api/image_downloader.rs
+++ src/api/image_downloader.rs
@@ -5,7 +5,9 @@ use reqwest::Response;
 
 use super::{model::ApiModel, model::ImageModel};
 
-pub async fn get_galleries_info(api_urls: &Vec<String>) -> Result<Vec<ApiModel>, Box<dyn std::error::Error>> {
+pub async fn get_galleries_info(
+    api_urls: &Vec<String>,
+) -> Result<Vec<ApiModel>, Box<dyn std::error::Error>> {
     let mut api_models: Vec<ApiModel> = Vec::new();
 
     for api_url in api_urls.iter() {
@@ -18,27 +20,38 @@ pub async fn get_galleries_info(api_urls: &Vec<String>) -> Result<Vec<ApiModel>,
     return Ok(api_models);
 }
 
-pub async fn get_images(api_model: &ApiModel) -> Result<Vec<ImageModel>, Box<dyn std::error::Error>> {
+pub async fn get_images(api_model: &ApiModel,) -> Result<Vec<ImageModel>, Box<dyn std::error::Error>> {
     let mut images: Vec<ImageModel> = Vec::new();
 
     let image_count: u64 = api_model.num_pages.try_into()?;
-    println!("Fetching {} images - {}", image_count, &api_model.title.pretty);
+    println!(
+        "Fetching {} images - {}",
+        image_count, &api_model.title.pretty
+    );
     let progress_bar = ProgressBar::new(image_count);
 
-    for page in 1..api_model.num_pages + 1 {
-        let target = format!("https://i.nhentai.net/galleries/{}/{}.jpg", api_model.media_id, page);
-        let response = reqwest::get(&target).await?;
-        
-        let file_name = generate_file_name(&response).await?;
-        let file_contents= response.bytes().await?;
-
-        let image_model = ImageModel{
-            file_name: file_name,
-            file_contents: file_contents,
+    for (i, page) in api_model.images.pages.iter().enumerate() {
+        let image_type = page.t.as_str();
+        let target = match image_type {
+            "j" => format!("https://i.nhentai.net/galleries/{}/{}.jpg", api_model.media_id, i + 1),
+            "p" => format!("https://i.nhentai.net/galleries/{}/{}.png", api_model.media_id, i + 1),
+            _ => format!("file format not found / supported"),
         };
 
-        images.push(image_model);
-        progress_bar.inc(1)
+        if target != "" {
+            let response = reqwest::get(&target).await?;
+
+            let file_name = generate_file_name(&response).await?;
+            let file_contents = response.bytes().await?;
+
+            let image_model = ImageModel {
+                file_name: file_name,
+                file_contents: file_contents,
+            };
+
+            images.push(image_model);
+            progress_bar.inc(1);
+        }
     }
 
     progress_bar.finish();
@@ -46,13 +59,13 @@ pub async fn get_images(api_model: &ApiModel) -> Result<Vec<ImageModel>, Box<dyn
     Ok(images)
 }
 
-async fn generate_file_name (response: &Response) -> Result<String, Box<dyn std::error::Error>>{
+async fn generate_file_name(response: &Response) -> Result<String, Box<dyn std::error::Error>> {
     let f_name = response
-    .url()
-    .path_segments()
-    .and_then(|segments| segments.last())
-    .and_then(|name| if name.is_empty() { None } else { Some(name) })
-    .unwrap_or("bigtiddy.bin");
+        .url()
+        .path_segments()
+        .and_then(|segments| segments.last())
+        .and_then(|name| if name.is_empty() { None } else { Some(name) })
+        .unwrap_or("bigtiddy.bin");
 
     let file_name = String::from(f_name);
 
